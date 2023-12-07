acea984ce804dac008d4f6d89fd27bd1ba2ec525
Nick Hirche <info@hirche-interactive.de>
2023-12-07 13:24:24 +0100

    marqueeslider changes

--- tt-scripts.js
+++ tt-scripts.js
@@ -1,125 +1,76 @@
-var swiper = new Swiper('.swiper-plays-slider', {
-    // Default parameters
-    slidesPerView: 1,
-    spaceBetween: 0,
-    // Responsive breakpoints
-    breakpoints: {
-        // when window width is >= 480px
-        600: {
-        slidesPerView: 2,
-        spaceBetween: 0,
-        },
-        // when window width is >= 640px
-        960: {
-        slidesPerView: 3,
-        spaceBetween: 0,
-        },
-    },
-    // Navigation
-    navigation: {
-        nextEl: ".swiper-button-next.plays",
-        prevEl: ".swiper-button-prev.plays",
-    },
-});
+let originalElements = {};
+let containers = {};
+let windowWidth = window.innerWidth;
 
-var swiper = new Swiper('.swiper-advanced-plays-slider', {
-    // Default parameters
-    slidesPerView: 1,
-    spaceBetween: 0,
-    // Responsive breakpoints
-    breakpoints: {
-        // when window width is >= 480px
-        600: {
-        slidesPerView: 2,
-        spaceBetween: 0,
-        },
-        // when window width is >= 640px
-        960: {
-        slidesPerView: 3,
-        spaceBetween: 0,
-        },
-    },
-    // Navigation
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
+function setAnimationDurationsAndClone(originalElement, container) {
+  // Berechne und setze die Animation-Dauer für das Original-Element
+  const pps = originalElement.getAttribute('data-pps');
+  if (pps && !isNaN(pps) && pps > 0) {
+    const duration = originalElement.offsetWidth / pps;
+    originalElement.style.animationDuration = `${duration}s`;
+  } else {
+    console.error('Invalid or missing data-pps attribute value for element:', originalElement);
+  }
 
-var swiper = new Swiper('.swiper-story-slider', {
-    // Default parameters
-    slidesPerView: 1,
-    spaceBetween: 0,
-    loop: true,
-    loopedSlides: 6,
-    speed: 400,
-    
-    autoplay: {
-        delay: 2500,
-        pauseOnMouseEnter: true,
-                disableOnInteraction: false,
-        disableOnInteraction: false,
-    },
-});
-var swiper = new Swiper('.swiper-challenges-slider', {
-    // Default parameters
-    slidesPerView: 1,
-    spaceBetween: 0,
-    // Responsive breakpoints
-        breakpoints: {
-      // when window width is >= 480px
-      660: {
-        slidesPerView: 2,
-        spaceBetween: 0,
-      },
-      // when window width is >= 640px
-      960: {
-        slidesPerView: 3,
-        spaceBetween: 0,
-      },
-      },
-    // Navigation
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+  let totalWidth = originalElement.offsetWidth;
+
+  // Füge Klone hinzu, bis die Gesamtbreite das 1,2-fache der Fensterbreite erreicht oder überschreitet
+  while (totalWidth <= windowWidth * 1.2) {
+    const clone = originalElement.cloneNode(true);
+    container.appendChild(clone);
+    totalWidth += clone.offsetWidth;
+  }
+
+  // Füge einen oder zwei finale Klone hinzu, sobald die Gesamtbreite das 1,2-fache der Fensterbreite erreicht oder überschreitet
+  if (totalWidth > windowWidth * 1.2) {
+    const finalClone1 = originalElement.cloneNode(true);
+    container.appendChild(finalClone1);
+
+    if (originalElement.classList.contains('scroll-reverse')) {
+      const finalClone2 = originalElement.cloneNode(true);
+      container.appendChild(finalClone2);
+    }
+  }
+}
+
+const classes = ['.tt-marquee', '.tt-logowall-wrapper'];
+
+// Event Listener, der die Funktion aufruft, sobald das Dokument geladen ist
+document.addEventListener('DOMContentLoaded', () => {
+  classes.forEach(className => {
+    let elements = document.querySelectorAll(className);
+    elements.forEach((element, index) => {
+      const key = `${className}${index}`;
+
+      // Speichere das ursprüngliche Element und das Container-Element
+      originalElements[key] = element.cloneNode(true);
+      containers[key] = element.parentNode;
+
+      // Füge Klone hinzu und berechne die Animation-Dauer
+      setAnimationDurationsAndClone(element, containers[key]);
+    });
   });
-
-  var swiper = new Swiper('.swiper-partner-slider', {
-    // Default parameters
-    spaceBetween: 0,
-    centeredSlides: true,
-    speed: 4000,
-    autoplay: {
-      delay: 0,
-    },
-    loop: true,
-    loopedSlides: 4,
-    slidesPerView:'auto',
-    allowTouchMove: false,
-    disableOnInteraction: true,
-    // Responsive breakpoints
-    breakpoints: {
-        // when window width is >= 480px
-        660: {
-            speed: 3000,
-        },
-    },
-  });
-
-// Piano Plays Slider Play Card Facts
-var swiper = new Swiper('.swiper-piano-plays-slider', {
-    // Default parameters
-    spaceBetween: 0,
-    speed: 30000,
-    autoplay: {
-        delay: 0,
-    },
-    loop: true,
-    loopedSlides: 6,
-    slidesPerView:'auto',
-    allowTouchMove: false,
-    disableOnInteraction: true,
 });
 
-swiper.controller.control = this.swiper2;
+// Event Listener, der die Funktion aufruft, wenn das Fenster neu skaliert wird
+window.addEventListener('resize', () => {
+  // Überprüfe, ob sich die Fensterbreite tatsächlich geändert hat
+  if (window.innerWidth !== windowWidth) {
+    windowWidth = window.innerWidth;
+
+    Object.keys(originalElements).forEach(key => {
+      const container = containers[key];
+      const originalElement = originalElements[key];
+
+      // Entferne alle Klone
+      while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+
+      // Füge das ursprüngliche Element wieder ein und füge dann Klone hinzu
+      const newElement = originalElement.cloneNode(true);
+      container.appendChild(newElement);
+      setAnimationDurationsAndClone(newElement, container);
+    });
+  }
+});
