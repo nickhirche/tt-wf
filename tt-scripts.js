b8f5775e43a5c871de838395bebd02009a3fb5b0
Nick Hirche <info@hirche-interactive.de>
2024-03-05 22:09:43 +0100

    Marquee Slider Update

--- tt-scripts.js
+++ tt-scripts.js
@@ -338,6 +338,33 @@ document.addEventListener('DOMContentLoaded', function() {
           
       // Weitere Konfigurationen können hier hinzugefügt werden
     };
+
+    // Allgemeine Optionen für den Marquee Slider
+    var baseMarqueeOptions = sliderOptions['.tt-marquee-slider'];
+
+    // Erstelle Slider-Instanzen für jeden .tt-marquee-slider
+    document.querySelectorAll('.tt-marquee-slider').forEach(function(sliderElement) {
+        // Lese den Wert von data-speed für den aktuellen Slider
+        var marqueeSpeed = sliderElement.dataset.speed;
+        marqueeSpeed = marqueeSpeed ? parseInt(marqueeSpeed, 10) : 2; // Verwende 2 als Standardwert, falls kein Wert gesetzt ist
+
+        // Kopiere allgemeine Optionen und überschreibe autoScroll.speed mit dem individuellen Wert
+        var marqueeOptions = {
+            ...baseMarqueeOptions,
+            autoScroll: {
+                ...baseMarqueeOptions.autoScroll,
+                speed: marqueeSpeed
+            }
+        };
+
+        // Erstelle eine neue Splide-Instanz mit den angepassten Optionen für den aktuellen Slider
+        var splideInstance = new Splide(sliderElement, marqueeOptions).mount(window.splide.Extensions);
+        
+        // Füge die neue Instanz zu einer Array von Slidern hinzu, basierend auf der Klasse
+        var sliderClass = '.tt-marquee-slider';
+        sliders[sliderClass] = sliders[sliderClass] || [];
+        sliders[sliderClass].push(splideInstance);
+    });
   
     // Array von Objekten, die definieren, welche Slider synchronisiert werden sollen
     var syncPairs = [
